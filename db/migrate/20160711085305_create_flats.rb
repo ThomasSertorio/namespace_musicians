3987314bb719b2f28a316ebbecec6f9cfeb3216d
Thomas Sertorio <thomas.sertorio@gmail.com>
2016-07-11 11:04:02 +0200

    adding red

---
+++ db/migrate/20160711085305_create_flats.rb
@@ -0,0 +1,9 @@
+class CreateFlats < ActiveRecord::Migration
+  def change
+    create_table :flats do |t|
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
